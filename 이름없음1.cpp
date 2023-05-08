939f417e59557ce1b0f3ebbef1acfd6843154474
seungpyo_sun <109188473+kingwangjjang30@users.noreply.github.com>
2023-05-08 10:52:15 +0900

    _khbit()(2)

---
+++ 이름없음1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char c;
+	while(1)
+	{
+		c=_getch();
+		printf("ÀÔ·Â°ª : %c\n",c);
+	}
+}
---
+++ 이름없음2.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
+
+int main(void) {
+    char c;
+    for (;;) {
+        if (_kbhit()) {      
+            c = _getch(); 
+            if (c == -32) {   
+                c = _getch();  
+                switch (c) {
+                case LEFT:
+                    printf("왼쪽으로 이동\n");
+                    break;
+                case RIGHT:
+                    printf("오른쪽으로 이동\n");
+                    break;
+                case UP:
+                    printf("위로 이동\n");
+                    break;
+                case DOWN:
+                    printf("아래로 이동\n");
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
